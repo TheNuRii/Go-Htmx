569bbe8f8b55010fd13d011797b14a3b0e6e836c
TheNuRii <maciekglow0@gmail.com>
2023-11-24 00:42:56 +0100

    Add adding functionality to the film list

--- main.go
+++ main.go
@@ -20,13 +20,22 @@ func main() {
 		films := map[string][]Film{
 			"Films": {
 				{Title: "The Godfather", Director: "Fracncis Ford Coppola"},
-				{Title: "Blade Runer", Director: "Rideley Scott"},					
+				{Title: "Blade Runer", Director: "Ridley Scott"},					
 			},
 		}
 
 		tmpl.Execute(w, films)
 	}
+	h2 := func (w http.ResponseWriter, r *http.Request) {
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+		htmlStr := fmt.Sprintf("<li class='list-group-item bg-primary text-white'>%s - %s</li>", title, director)
+		tmpl, _ := template.New("template").Parse(htmlStr)
+		tmpl.Execute(w, nil)
+	}
+
 	http.HandleFunc("/", h1)
+	http.HandleFunc("/add-film/", h2)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
